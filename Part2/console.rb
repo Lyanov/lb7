9a8383218f6850c3d2fdd7c6ad74749ee419fb15
Lyanov <46460803+Lyanov@users.noreply.github.com>
2019-01-08 17:56:20 +0300

    g

--- Part2/console.rb
+++
@@ -1,8 +0,0 @@
-require './circle.rb'
-
-point = Point.new 10, 3
-circle = Circle.new 3, 6, 10
-print "#{circle}\n"
-print "#{point}\n"
-print "#{point.pos_x} #{point.pos_y}\n"
-print "#{circle.pos_x} #{circle.pos_y} #{circle.radius}\n"
